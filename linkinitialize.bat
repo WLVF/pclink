235d6a0b4dfd8ff8e4a240a70efd3e67ce5a68f7
WLVF <65689785+WLVF@users.noreply.github.com>
2020-08-08 05:22:50 -0400

    Add files via upload

---
+++ linkinitialize.bat
@@ -0,0 +1,8 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+cd ..
+cd AppData
+cd Roaming
+cd discord
+cd pclink
+echo Please ask if the host is online.
+python client.py
